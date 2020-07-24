7ffcf91f8883a3ece7f4e9ae1974c164010727aa
72nd <32248252+72nd@users.noreply.github.com>
2020-07-24 17:11:21 +0200

    all font stuff into one go file

--- fonts/fonts.go
+++ fonts/fonts.go
@@ -14,10 +14,48 @@ type FontFamily struct {
 	Bold   []byte
 }
 
+func loadFromRice(filename, description string) ([]byte, error) {
+	box, err := riceBox()
+	if err != nil {
+		return nil, err
+	}
+	data, err := box.Bytes(filename)
+	if err != nil {
+		return nil, fmt.Errorf("could not load %s: %s", description, err)
+	}
+	return data, nil
+
+}
+
 func riceBox() (*rice.Box, error) {
 	box, err := rice.FindBox("utils")
 	if err != nil {
 		return nil, fmt.Errorf("rice find box failed: %s", err)
 	}
 	return box, nil
+}
+
+func NewLatoFamily() (*FontFamily, error) {
+	heavy, err := LatoHeavy()
+	if err != nil {
+		return nil, err
+	}
+	normal, err := LatoRegular()
+	if err != nil {
+		return nil, err
+	}
+	return &FontFamily{
+		Name:   "lato",
+		Normal: normal,
+		Bold:   heavy}, nil
+}
+
+// LatoHeavy returns the heavy style of the Lato font.
+func LatoHeavy() ([]byte, error) {
+	return loadFromRice("Lato-Heavy.ttf", "lato heavy")
+}
+
+// LatoRegular returns the regular style of the Lato font.
+func LatoRegular() ([]byte, error) {
+	return loadFromRice("Lato-Regular.ttf", "lato heavy")
 }

d5ba5dadaec352d4135dff6778d45f8788c0a3c9
72nd <32248252+72nd@users.noreply.github.com>
2020-07-24 16:21:52 +0200

    add font utils

---
+++ font.go
@@ -0,0 +1,16 @@
+package gopdf_wrapper
+
+import "github.com/signintech/gopdf"
+
+// FontStyle wraps the gopdf font style constants into a type.
+type FontStyle int
+
+const (
+	NormalStyle    FontStyle = gopdf.Regular
+	ItalicStyle              = gopdf.Italic
+	BoldStyle                = gopdf.Bold
+	UnderlineStyle           = gopdf.Underline
+)
+
+// fontFunction is a function which returns a font as bytes and an optional error.
+type fontFunction func() ([]byte, error)
